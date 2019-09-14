232632a5dc36d4789e9c6a2e5b006b3ff0ba28fd
kyung <leeseok8347@gmail.com>
2019-09-15 00:50:27 +0900

    edit bytes arrary for test

--- chaincode/util/testUtil.go
+++ chaincode/util/testUtil.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
@@ -22,11 +23,11 @@ func CheckState(t *testing.T, stub *shim.MockStub, name string, value string) {
 func CheckQuery(t *testing.T, stub *shim.MockStub, args [][]byte, expect string, txId string) {
 	res := stub.MockInvoke(txId, args)
 	if res.Status != shim.OK {
-		fmt.Println("Query", args, "failed", string(res.Message))
+		fmt.Println("Query (", convertArgToString(args), ") failed", string(res.Message))
 		t.FailNow()
 	}
 	if res.Payload == nil {
-		fmt.Println("Query", args, "failed to get result")
+		fmt.Println("Query (", convertArgToString(args), ") failed to get result")
 		t.FailNow()
 	}
 	if string(res.Payload) != expect {
@@ -38,7 +39,16 @@ func CheckQuery(t *testing.T, stub *shim.MockStub, args [][]byte, expect string,
 func CheckInvoke(t *testing.T, stub *shim.MockStub, args [][]byte, txId string) {
 	res := stub.MockInvoke(txId, args)
 	if res.Status != shim.OK {
-		fmt.Println("Invoke", args, "failed", string(res.Message))
+		fmt.Println("Invoke (", convertArgToString(args), ") failed: ", string(res.Message))
 		t.FailNow()
 	}
+}
+
+func convertArgToString(args [][]byte) string {
+	var strs []string
+	for _, v1 := range args {
+		arg := string(v1)
+		strs = append(strs, arg)
+	}
+	return strings.Join(strs, ", ")
 }
